ac3d19020e550dab1fddb5d3bfcf1f96d13f4a6b
Daniel Saad Nogueira Nunes <daniel.saad.nunes@gmail.com>
2017-11-21 10:25:09 -0200

    adicionando exercicios de recursividade

---
+++ Listas de Exercício/Recursividade/ex1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void imprime_rec(int* v,int i,int n){
+    if(i==n){
+        return;
+    }
+    printf("v[%d] = %d\n",i,v[i]);
+    imprime_rec(v, i+1, n);
+}
+
+int main(void){
+    int n, *v;
+    int i;
+    printf("Digite o nro de elementos do vetor: ");
+    scanf("%d",&n);
+    v = calloc(n,sizeof(int));
+    for(i=0;i<n;i++){
+        printf("v[%d] = ",i);
+        scanf("%d",&v[i]);
+    }
+    imprime_rec(v,0,n);
+}
